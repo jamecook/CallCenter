5c40fe419d9945a915bf1482b9b43cf97f2f663c
aserzerg <aserzerg@mail.ru>
2017-06-04 00:41:48 +0500

    [~] - Add Wcf

---
+++ RequestWcfService/IRequestWebService.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using RequestServiceImpl.Dto;
+
+namespace RequestWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IRequestWebService
+    {
+        [OperationContract]
+        CityDto[] GetData();
+     }
+}
---
+++ RequestWcfService/RequestWebService.svc.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using MySql.Data.MySqlClient;
+using RequestServiceImpl;
+using RequestServiceImpl.Dto;
+
+
+namespace RequestWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RequestWebService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RequestWebService.svc or RequestWebService.svc.cs at the Solution Explorer and start debugging.
+    public class RequestWebService : IRequestWebService
+    {
+        private RequestService _requestService;
+        private MySqlConnection _connection;
+        public RequestWebService()
+        {
+            var connectionString = string.Format("server={0};uid={1};pwd={2};database={3};charset=utf8", "192.168.1.130", "asterisk", "mysqlasterisk", "asterisk");
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+            _requestService = new RequestService(_connection);
+        }
+        public CityDto[] GetData()
+        {
+            return _requestService.GetCities().ToArray();
+        }
+    }
+}
