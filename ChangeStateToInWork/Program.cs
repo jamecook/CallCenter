a2220814fe99767314e4abb8e1ea899323658f1d
aserzerg <aserzerg@mail.ru>
2018-06-08 23:17:38 +0500

    [~] - ChangeStateToInWork

---
+++ ChangeStateToInWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace ChangeStateToInWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverIP = ConfigurationManager.AppSettings["CallCenterIP"];
+            var connectionString = string.Format("server={0};uid={1};pwd={2};database={3};charset=utf8", serverIP,
+                "asterisk", "mysqlasterisk", "asterisk");
+            var conn = new MySqlConnection(connectionString);
+            conn.Open();
+            using (var cmd = new MySqlCommand("call CallCenter.RequestChangeStateToInWork()", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
